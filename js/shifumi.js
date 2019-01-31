d34f0e2ce35dd217c076c324031fe929676d4c44
Laureline <laurelinedev@gmail.com>
2019-01-31 18:10:09 +0100

    Main adjustement added

--- js/shifumi.js
+++ js/shifumi.js
@@ -1,6 +1,6 @@
 let scores = { user : 0, cpu : 0 };
 let messages = {
-    play: ['Do you want to play?', `Do you want to loose?`, 'Are you ready for the next battle?', 'Do you want to fight?', 'Do you want to give it a shot?!', `Don't you want to loose against a mere computer?`, 'Do you want to bet on who will be superior?!', 'Play with me?!'],
+    play: ['Do you want to play?', `Do you want to loose?`, 'Are you ready for the next battle?', 'Do you want to fight?', 'Do you want to give it a shot?!', `Don't you want to loose against a mere computer?`, 'Do you want to bet on who will be superior?!', 'Would you play with me?!'],
     win : ['Well done, for now!', 'I saw you cheated maannn!', 'Yeah, okay, I give it to you.', 'Hey, jayjaaay (GG)!',`I'll never give up against a human!`, 'Pfff, never mind.',`Come on, again?! Really?!`, `Don't you want to play fair for once?!`],
     lost : ['Ohhh, sorry... or not.', 'Maybe next time?!', 'Keep trying...', `I'm afraid you just lost...`, 'Ahhhh... Nope, you lost!', 'The computer seems to be smarter than a human.', 'Bouuuuyyyaaahh!!','Poly owned!'],
     draw : ['No wins, no losses.', 'Draw! Play again?!', 'And this is... uh?! A tie.', `It's a tie!`, 'Same weapon, duh!', `Next time will be the good one?`, 'One more chance, yeahh!', 'Noooooooooooo, a draw!']
@@ -12,7 +12,7 @@ $('document').ready( function(){
 
 function generateMessage( status ) {
     var index = Math.floor( Math.random() * messages[status].length );
-    return messages[status][index];
+    return `"${messages[status][index]}"`;
 }
 
 function computerPlay() {
@@ -53,8 +53,9 @@ function versusResult( userWeapon, computerWeapon ) {
     }
 }
 
-function reset() {
+function resetScore() {
     scores.user = scores.cpu = 0;
+    setScores();
 }
 
 function polyOwned() {
@@ -63,6 +64,8 @@ function polyOwned() {
     }
 }
 
-document.querySelector('.reset').addEventListener('click', () => {
+document.querySelector('.resetScore').addEventListener('click', () => {
     scores.user = scores.cpu = 0;
+    setScores();
+
 })
